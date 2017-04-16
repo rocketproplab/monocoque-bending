5a5ce2a4da21cc6907b30627dd8550ab7e929781
alexander-lim <alexlim2118@gmail.com>
2017-04-16 14:19:38 -0700

    Accounting for different radii
    Radius is now a vector from 3in-6in,  Now plots graphs of radius vs.
    bending moment,

--- monocoque-bending.py
+++ monocoque-bending.py
@@ -1,42 +1,57 @@
+
+# coding: utf-8
+
+# In[39]:
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def semimonocoque():
-    return 0
+        return 0
 
 
 def monocoque():
-    SFy = 1.25
-    SFu = 1.5  # factor of safety, ultimate
+        SFy = 1.25
+        SFu = 1.5  # factor of safety, ultimate
 
-    # material properties
-    rho = 0.0975  # lb/in^3
-    sigmay = 19000  # psi
-    sigmau = 33000  # psi
+        # material properties
+        rho = 0.0975  # lb/in^3
+        sigmay = 19000  # psi
+        sigmau = 33000  # psi
+    
+        sigmastar = min(sigmay/SFy, sigmau/SFu)
 
-    sigmastar = min(sigmay/SFy, sigmau/SFu)
+        # airframe calculations
+        length = 36  # length of monocoque section
+        radius_mean = np.linspace(3,6)  # in
+        thickness = np.linspace(0.125, .5, 4)
 
-    # airframe calculations
-    length = 36  # length of monocoque section
-    radius_mean = 6  # in
-    thickness = np.linspace(0.125, 0.5)  # in
+        for x in thickness:
+            thick = x
+            circumference = 2*np.pi*radius_mean
+            mass = circumference * thick * length * rho  # mass of cylinder
+            # moment of inertia of the cylinder
+            inertia = mass * radius_mean**2
+            y=length
 
-    circumference = 2*np.pi*radius_mean
-    mass = circumference * thickness * length * rho  # mass of cylinder
-    # moment of inertia of the cylinder
-    inertia = mass * radius_mean**2
-
-    # calculating stresses
-    sigma = moment * y / I
-    moment = sigma * I / y
-    sigma
-
-
+            # calculating stresses
+            #sigma = moment * y / I
+            moment = sigmastar * inertia / y
+            print(thick)
+            plt.plot(radius_mean, moment)
+            plt.show()
+                
 def main():
-    print(monocoque())
-    print(semimonocoque())
+        print(monocoque())
+        print(semimonocoque())
 
 
 if __name__ == '__main__':
-    main()
+        main()
+
+
+# In[ ]:
+
+
+
